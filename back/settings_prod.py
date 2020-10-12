6d7d58ac26a64032e03d807a375dd3deca2b53f3
ignite7 <ignite21@protonmail.com>
2020-10-12 16:56:04 +0200

    changes settings prod

--- back/settings_prod.py
+++ back/settings_prod.py
@@ -30,7 +30,6 @@ TITLE = 'The Back Project | '
 # Application definition
 DJANGO_APPS = [
     'django.contrib.admin',
-    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
